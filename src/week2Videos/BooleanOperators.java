d6bd217b0c20afb2e8d5a6a2635b38867064b2a5
AshaAbdu <halge87@yahoo.com>
2022-07-03 21:31:44 -0500

    1st commit BooleanOperators

---
+++ src/week2Videos/BooleanOperators.java
@@ -0,0 +1,24 @@
+package week2Videos;
+
+public class BooleanOperators {
+
+	public static void main(String[] args) {
+		// == Equality operator
+		
+		System.out.println(2==2);
+		System.out.println(2>5);
+		System.out.println(3<=5);
+		System.out.println(5<=5);
+		
+		
+		// && and || or
+		 System.out.println( 2 == 2 && 1<5 );
+		System.out.println(true && true);
+		System.out.println(true && false);
+		System.out.println(true || false);
+		System.out.println(false || false);
+		System.out.println(2 == 2 && 1 == 5);
+		
+	}
+
+}
